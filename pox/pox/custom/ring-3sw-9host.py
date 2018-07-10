e73a0fee842df3f481c96af8e9684f5d5252bbea
jorgelukas <lukasbandarra@gmail.com>
2018-07-09 18:25:07 -0700

    Finalizacao controller. Substituicao de ifs por for's aninhados para selecao de rede e porta switch

--- pox/pox/custom/ring-3sw-9host.py
+++ pox/pox/custom/ring-3sw-9host.py
@@ -25,48 +25,31 @@ Three directly connected switches plus three hosts for each one:
 from pox.core import core
 import pox.openflow.libopenflow_01 as of
 import pox.lib.packet as pkt
-from pox.lib.util import dpidToSTr
+from pox.lib.util import dpidToStr
 
 
-def _handle_PacketIn (event):
-	match = of.ofp_match(event.ofp, event.port)#Match do PacketIn
+def _handle_PacketIn (Event):
+	match = of.ofp_match.from_packet(Event.ofp,Event.port)#Match do PacketIn 
+
 	nw_src = match.get_nw_src()#Ip de quem envia o packet
-	nw_dsr = match.get_nw_dst()#IP de quem recebe o packet
-	dpid = match.dpid
+	nw_dst = match.get_nw_dst()#IP de quem recebe o packet
+#	dpid = match.ofp.dpid
+	dpid = Event.connection.dpid #match com forca
 
-	msg = of.ofp_flow_mod()#Encapsula o comando para criação de uma tabela de entrada no switch
+	msg = of.ofp_flow_mod()#Encapsula o comando para criacao de uma tabela de entrada no switch
 
-	# Se o switch 1 requisitou a regra e teve um missing para um pacote na sua própria rede
-	if( (dpid == 1)  && ( nw_src == IPAddr("10.10.1.0",24) ) ):
-		
+
+	# Se o switch i requisitou a regra e teve um missing para um pacote na sua propria rede
+	for i in xrange(1,4):
+		for j in xrange(1,4):
+			if( (dpid == i) and ( nw_src == IPAddr("10.10.%d.%d" %(i) %(j),24) ) ):
+				msg.actions.append(of.ofp_action_output(port = j))
+				msg.match.set_nw_dst(match.get_nw_dst())
+
+	Event.connection.send(msg)
+	#msg.match.set_nw_dst(IPAddr("10.10.1.0"),24)
+	#msg.match.set_nw_dst(IPAddr("10.10.2.0"),24)
+	#msg.match.set_nw_dst(IPAddr("10.10.3.0"),24)
 	
-	# Se o switch 1 requisitou a regra e teve um missing para um pacote na rede do switch 2	
-	elif((dpid == 1)  && ( nw_src == IPAddr("10.10.2.0",24) ) ):
-
-        # Se o switch 1 requisitou a regra e teve um missing para um pacote na rede do switch 3
-	elif((dpid == 1)  && ( nw_src == IPAddr("10.10.3.0",24) ) ):
-
-	# Se o switch 2 requisitou a regra e teve um missing para um pacote na rede do switch 1	
-	elif((dpid == 2)  && ( nw_src == IPAddr("10.10.1.0",24) ) ):
-
-	# Se o switch 2 requisitou a regra e teve um missing para um pacote na sua própria rede 
-	elif((dpid == 2)  && ( nw_src == IPAddr("10.10.2.0",24) ) ):
-
-	# Se o switch 2 requisitou a regra e teve um missing para um pacote na rede do switch 3
-	elif((dpid == 2)  && ( nw_src == IPAddr("10.10.3.0",24) ) ):
-
-	# Se o switch 3 requisitou a regra e teve um missing para um pacote na rede do switch 1
-        elif((dpid == 3)  && ( nw_src == IPAddr("10.10.1.0",24) ) ):
-
-	# Se o switch 3 requisitou a regra e teve um missing para um pacote na rede do switch 2
-        elif((dpid == 3)  && ( nw_src == IPAddr("10.10.2.0",24) ) ):
-
-	# Se o switch 3 requisitou a regra e teve um missing para um pacote na sua própria rede
-        elif((dpid == 3)  && ( nw_src == IPAddr("10.10.3.0",24) ) ):
-
-
-
-	msg.match.set_nw_dst(IPAddr("10.10.1.0"),24)
-	msg.match.set_nw_dst(IPAddr("10.10.2.0"),24)
-	msg.match.set_nw_dst(IPAddr("10.10.3.0"),24)
-	
+def launch (reactive = False):
+    core.openflow.addListenerByName("PacketIn", _handle_PacketIn)
