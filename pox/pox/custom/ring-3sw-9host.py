db2ba2dd42fcdc31f00662ab2327b9514655a089
jorgelukas <lukasbandarra@gmail.com>
2018-07-11 16:52:08 -0700

    Solucao ping funcionando h1 - h2

--- pox/pox/custom/ring-3sw-9host.py
+++ pox/pox/custom/ring-3sw-9host.py
@@ -28,36 +28,62 @@ import pox.lib.packet as pkt
 from pox.lib.util import dpidToStr
 from pox.lib.addresses import IPAddr, IPAddr6, EthAddr
 
-def _handle_PacketIn (Event):
-	print "Entrou Handle Packet In"
-	match = of.ofp_match.from_packet(Event.ofp,Event.port)#Match do PacketIn 
+def _handle_PacketIn (event):
+#	print "Entrou Handle Packet In"
+# 	match = of.ofp_match.from_packet(Event.ofp,Event.port)#Match do PacketIn 
 
-	nw_src = match.get_nw_src()#Ip de quem envia o packet
-	nw_dst = match.get_nw_dst()#IP de quem recebe o packet
-	dpid = Event.connection.dpid #match com forca
+# 	nw_src = match.get_nw_src()#Ip de quem envia o packet
+# 	nw_dst = match.get_nw_dst()#IP de quem recebe o packet
+# 	dpid = Event.connection.dpid #match com forca
 
-	msg = of.ofp_flow_mod()#Encapsula o comando para criacao de uma tabela de entrada no switch
-
-	print "Ip de Destino = " + str(nw_dst)
-	# Se o switch i requisitou uma regra, o controlador o orienta a enviar o pacote na porta j
-	for i in xrange(1,4):
-		print "Entrou for i"
-		for j in xrange(1,4):
-			print "Entrou for j"
-			ipBase = "10.10." + str(i) + "." + str(j)
-			ip = repr(ipBase)
-			ip ="(IPAddr(" +  ip  +"), 32)" 
-			print "Testando IP de Destino para " + ip
-			if( (dpid == i) and ( str(nw_dst) == ip ) ):
-				print "Entrou if"
-				print "IP:" +  ip
-				msg.actions.append(of.ofp_action_output(port = j))
-				msg.match.set_nw_dst(IPAddr(ipBase), 32)
-				Event.connection.send(msg)
+# 	print "Ip de Destino = " + str(nw_dst)
+# 	# Se o switch i requisitou uma regra, o controlador o orienta a enviar o pacote na porta j
+# 	for i in xrange(1,4):
+# 		print "Entrou for i"
+# 		for j in xrange(1,4):
+# 			print "Entrou for j"
+# 			ipBase = "10.10." + str(i) + "." + str(j)
+# 			ip = repr(ipBase)
+# 			ip ="(IPAddr(" +  ip  +"), 32)" 
+# 			print "Testando IP de Destino para " + ip
+# 			if( (dpid == i) and ( str(nw_dst) == ip ) ):
+# 				print "Entrou if"
+# 				print "IP:" +  ip
+# 				msg = of.ofp_flow_mod()#Encapsula o comando para criacao de uma tabela de entrada no switch
+# 				msg.match.dl_type = 0x800
+# 				msg.match.set_nw_dst(IPAddr("10.10.1.2"), 32)
+# 				msg.actions.append(of.ofp_action_output(port = 2))
+# 				Event.connection.send(msg)
 
 	#msg.match.set_nw_dst(IPAddr("10.10.1.0"),24)
-	#msg.match.set_nw_dst(IPAddr("10.10.2.0"),24)
-	#msg.match.set_nw_dst(IPAddr("10.10.3.0"),24)
-	
+
+	packet = event.parsed
+	match = of.ofp_match.from_packet(packet,event.port)#Match do PacketIn
+	print "MAC Fonte: " + str(match.dl_src)
+	print "MAC DEST: " + str(match.dl_dst)
+	nw_src = match.get_nw_src()#Ip de quem envia o packet
+	nw_dst = match.get_nw_dst()#IP de quem recebe o packet
+	print "IP Fonte: " + str(nw_src)
+	print "IP Destino: " + str(nw_dst)
+	if(event.port == 1):
+		#match.dl_dst = EthAddr("d2:de:85:42:a0:94")
+		msg = of.ofp_flow_mod()#Encapsula o comando para criacao de uma tabela de entrada no switch
+		msg.match.dl_type = 0x800
+		msg.match.nw_dst = IPAddr("10.10.1.2")
+		msg.data = event.ofp
+		#msg.match.dl_dst = EthAddr("3a:fe:0d:a2:bd:60")
+		msg.actions.append(of.ofp_action_output(port = 2))
+		event.connection.send(msg)
+
+	elif(event.port == 2):
+		#match.dl_dst = EthAddr("d2:de:85:42:a0:94")
+		msg = of.ofp_flow_mod()#Encapsula o comando para criacao de uma tabela de entrada no switch
+		msg.match.dl_type = 0x800
+		msg.match.nw_dst = IPAddr("10.10.1.1")
+		msg.data = event.ofp
+		#msg.match.dl_dst = EthAddr("3a:fe:0d:a2:bd:60")
+		msg.actions.append(of.ofp_action_output(port = 1))
+		event.connection.send(msg)
+
 def launch (reactive = False):
     core.openflow.addListenerByName("PacketIn", _handle_PacketIn)
